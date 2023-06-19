cea704b13d818c3ef71efc281c6460522d833f3b
Rodrigoneal <rodrigho2006@gmail.com>
2023-06-19 15:24:01 -0300

    Mudando o SeleniumDriver

--- selenium_tools/selenium_driver/webdriver.py
+++ selenium_tools/selenium_driver/webdriver.py
@@ -16,17 +16,30 @@ class SeleniumDriver:
     options.add_experimental_option("prefs", prefs)
     s = Service(ChromeDriverManager().install())
 
-    def get_driver(cls, download_path: Optional[str] = None,
-                   read_pdf: bool = True, log: bool = True) -> WebDriver:
-        if download_path:
-            cls.download_path = download_path
-            cls.prefs["download.default_directory"] = download_path
-        cls.prefs["plugins.always_open_pdf_externally"] = read_pdf
-        if not log:
+    def __init__(self,download_path: Optional[str] = None,
+                   read_pdf: bool = True, log: bool = True,
+                   headless: bool = False) -> None:
+        self.download_path = download_path
+        self.read_pdf = read_pdf
+        self.log = log
+        self.headless = headless
+        self._prime()
+    
+    def _prime(self):
+        if self.download_path:
+            self.prefs["download.default_directory"] = self.download_path
+        self.prefs["plugins.always_open_pdf_externally"] = self.read_pdf
+        if not self.log:
             LOGGER.setLevel(logging.ERROR)
             urllib_log.setLevel(logging.ERROR)
             options.add_argument("--log-level=3")
-        driver = webdriver.Chrome(service=cls.s, chrome_options=options)
-        driver.caps['options'] = download_path or options._caps.get(
+        if self.headless:
+            options.add_argument("--headless=new")
+
+
+
+    def get_driver(self) -> WebDriver:
+        driver = webdriver.Chrome(service=self.s, chrome_options=options)
+        driver.caps['options'] = self.download_path or options._caps.get(
             "goog:chromeOptions").get('prefs').get('download.default_directory')
         return driver
